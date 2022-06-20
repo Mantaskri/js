d74b0690eb1c536ed6c58492d28c128810c60b7e
Mantas Krinickas <mantas.krinickas@hotmail.com>
2022-06-20 13:29:43 +0300

    edit

--- js/if-values.js
+++ js/if-values.js
@@ -12,7 +12,7 @@ object : visada pozityvus;
 function() {} : visada pozityvus;
 arrow function : -//- visada pozityvus;
 */
-
+/*
 const userInput = ' ';
 
 if (userInput) {
@@ -20,3 +20,14 @@ if (userInput) {
 } else {
     console.log('ne');
 }
+*/
+
+
+let multi = 3;
+let str = `"variable1", "variable2"`;
+let multiStr = "";
+
+for(let i = 0; i < multi; i++){
+  multiStr += str
+}
+console.log(multiStr);
